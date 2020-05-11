77ebe8d98fe238f221d3ba6bb72e349f8474fadc
jsbravo-sw <42355818+jsbravo-sw@users.noreply.github.com>
2020-05-11 15:57:55 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-   res.redirect("http://localhost:3000");
+   res.redirect("https://spyfall-webapp.herokuapp.com/");
 });
 
 module.exports = router;
